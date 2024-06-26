e52abac48375d392e83f858a739855448eb962f6
Chukwudumebi Onwuli <37223065+deeonwuli@users.noreply.github.com>
2024-06-26 11:36:57 +0100

    chore: use default app namespace

--- src/webapp/contexts/app-context.ts
+++ src/webapp/contexts/app-context.ts
@@ -2,6 +2,8 @@ import React, { useContext } from "react";
 import { CompositionRoot } from "../../CompositionRoot";
 import { User } from "../../domain/entities/User";
 import { D2Api } from "../../types/d2-api";
+// @ts-ignore
+import i18n from "../../locales";
 
 export interface AppContextState {
     api: D2Api;
@@ -14,6 +16,7 @@ export const AppContext = React.createContext<AppContextState | null>(null);
 
 export function useAppContext() {
     const context = useContext(AppContext);
+    i18n.setDefaultNamespace("d2-multi-visualization");
     if (context) {
         return context;
     } else {
